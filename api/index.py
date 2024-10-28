5fa94a3890401168c3665fc8f566df0a611e3b3a
REVYID <revy8k@gmail.com>
2024-10-28 15:37:41 +0700

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,9 +1,14 @@
 from http.server import BaseHTTPRequestHandler
+import json
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
+        # mendapatkan alamat IP pengguna dari request
+        user_ip = self.headers.get('X-Forwarded-For', self.client_address[0])
+        response = {"ip": user_ip}
+
+        # mengatur respons menjadi JSON
         self.send_response(200)
-        self.send_header('Content-type', 'text/plain')
+        self.send_header('Content-type', 'application/json')
         self.end_headers()
-        self.wfile.write('Hello, world!'.encode('utf-8'))
-      
+        self.wfile.write(json.dumps(response).encode('utf-8'))
